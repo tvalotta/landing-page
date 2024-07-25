8c99bfcc7888b9d71638177810b6cc917556f7d5
Thomas <tommyvalotta@gmail.com>
2024-07-25 16:54:07 -0400

    Created skill section carousel. Added a few contact links.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+const prev = document.getElementById('prev-btn');
+const next = document.getElementById('next-btn');
+const list = document.getElementById('item-list');
+const itemWidth = 150;
+const padding = 10;
 
-buttons.forEach(button => {
-    button.addEventListener("click", ()=>{
-        const offset = button.dataset.carouselButton == "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
-        const activeSlide = slides.querySelector('[data-active]')
+prev.addEventListener('click', () => {
+    list.scrollLeft -= itemWidth + padding
+})
 
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if(newIndex < 0) newIndex = slides.children.length - 1
-        if(newIndex >= slides.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
+next.addEventListener('click', () => {
+    list.scrollLeft += itemWidth + padding
 })
